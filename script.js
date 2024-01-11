79cd31ca55253e586f8d0510477c2faae8666d92
nad_cat <130969010+npelcat@users.noreply.github.com>
2024-01-11 12:41:39 +0100

    légères modifs du script - fix bug affichage

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const inputName = document.getElementById("personName")
-
-let myButton = docum.getElementById("bouton")
+let myButton = document.getElementById("bouton");
 
 //const inputName = "Chuck Norris"
 
 async function apiWiki(name) {
+  let urlApi = `https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`;
 
-    try {
-        let urlApi = `https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`;
-    
-        const response = await fetch(urlApi);
-    
-        if(!response.ok) {
-            throw new console.error(`HTTP error! Status: ${response.status}`);
-        }
-    
-        const data = await response.json();
-        //console.log(data);
-        const personPicture = data.originalimage.source;
-        console.log(personPicture);
+  try {
+    const response = await fetch(urlApi);
 
-        const personName = data.titles.normalized;
-        console.log(personName);
-
-        const personBio = data.extract;
-        console.log(personBio)
-    } catch (error) {
-        console.error("Error fetching data:", error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
+    const data = await response.json();
+    //console.log(data);
+    const personPicture = data.originalimage.source;
+    console.log(personPicture);
+
+    const personName = data.titles.normalized;
+    console.log(personName);
+
+    const personBio = data.extract;
+    console.log(personBio);
+  } catch (error) {
+    console.error("Error fetching data:" + error.message);
+  }
 }
 
-apiWiki(inputName);  
-
-myButton.addEventListern("click", () => {
-
-})
+myButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputName = document.getElementById("personName").value;
+  const result = apiWiki(inputName);
+  console.log(result);
+});

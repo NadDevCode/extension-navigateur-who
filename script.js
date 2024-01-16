5011d10ddb2d66f47a32eba14e7c9f07e34fee33
nad_cat <130969010+npelcat@users.noreply.github.com>
2024-01-16 14:18:15 +0100

    ajout message d'erreur pour le 404

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+//BUTTON
 let myButton = document.getElementById("bouton");
+
+//DIVS HTML
+const personPicture = document.getElementById("picture");
+const personName = document.getElementById("name");
+const personBio = document.getElementById("description");
 
 async function apiWiki(name) {
   let urlApi = `https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`;
@@ -11,7 +17,6 @@ async function apiWiki(name) {
     }
 
     const data = await response.json();
-
     let pictureDisplay = "";
 
     if (name === "Eric_Zemmour") {
@@ -20,26 +25,23 @@ async function apiWiki(name) {
       pictureDisplay = data.originalimage.source;
     }
 
-    const personPicture = document.getElementById("picture");
     personPicture.innerHTML = `<img src="${pictureDisplay}" style='width: 200px; height: auto'>`;
-
-    const personName = document.getElementById("name");
     personName.innerHTML = `<a href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
-
-    const personBio = document.getElementById("description");
     personBio.innerHTML = data.extract;
   } catch (error) {
+    personName.innerHTML = `Oups ! Cette personne ne semble pas exister...`;
+    personPicture.innerHTML = `<img src="./icons/giphy.gif" style='width: 200px; height: auto'>`;
+    personBio.innerHTML = "";
+
     console.error("Error fetching data:" + error.message);
   }
 }
 
 const formatName = (inputValue) => {
   const newName = inputValue.split(" ");
-
   for (var i = 0; i < newName.length; i++) {
     newName[i] = newName[i].charAt(0).toUpperCase() + newName[i].slice(1);
   }
-
   const nameForApi = newName.join(" ").replace(" ", "_");
 
   return nameForApi;
@@ -48,12 +50,11 @@ const formatName = (inputValue) => {
 myButton.addEventListener("click", async (e) => {
   e.preventDefault();
   const inputName = document.getElementById("personName").value;
-
   const resultFormatName = formatName(inputName);
 
   try {
     await apiWiki(resultFormatName);
-  } catch {
-    console.error("Error:" + error);
+  } catch (error) {
+    console.error("Error:", error);
   }
 });

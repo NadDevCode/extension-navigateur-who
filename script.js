8131d971cd6c71f4ea94d96172cad364bd77780a
nad_cat <130969010+npelcat@users.noreply.github.com>
2024-01-17 16:14:36 +0100

    dernière modifs css

--- script.js
+++ script.js
@@ -11,24 +11,48 @@ async function apiWiki(name) {
 
   try {
     const response = await fetch(urlApi);
+    console.log(response);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     const data = await response.json();
-    let pictureDisplay = "";
+    console.log(data.originalimage);
 
-    if (name === "Eric_Zemmour") {
-      pictureDisplay = "https://urlz.fr/pfmI";
+    if (data.originalimage) {
+      let pictureDisplay;
+      let bioDisplay;
+
+      switch (name) {
+        case "Eric_Zemmour":
+          pictureDisplay = "https://urlz.fr/pfmI";
+          bioDisplay =
+            "Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration ImmigrationImmigration Immigration  Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration ImmigrationImmigration  Immigration Immigration Immigration Immigration Immigration Immigration Immigration";
+          break;
+        case "Cyril_Hanouna":
+          pictureDisplay = "https://urlr.me/MvbCP";
+          bioDisplay = `né le 23 septembre 1974
+          Animateur incontesté du chaos médiatique. Avec une subtilité aussi rare qu'une licorne rose, Hanouna a réussi à transformer chaque émission en un « spectacle » où la décence et la logique font leurs adieux.ssion en un « spectacle » où la décence et la logique font leurs adieux.`;
+          break;
+        case "Emmanuel_Macron":
+          pictureDisplay =
+            "https://confpeps.org/wp-content/uploads/2023/03/macron-doigt-retraite-750x465.jpg";
+          break;
+        default:
+          pictureDisplay = data.originalimage.source;
+          bioDisplay = data.extract;
+      }
+
+      personPicture.innerHTML = `<img src="${pictureDisplay}" style='width: 200px; height: auto; border-radius: 10px'>`;
+      personName.innerHTML = `<a href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
+      personBio.innerHTML = bioDisplay;
     } else {
-      pictureDisplay = data.originalimage.source;
+      personBio.innerHTML = `<i id="hand" class="fa-solid fa-hand-pointer"></i> <br/> Il existe plusieurs liens pour ce nom. Débrouillez-vous !`;
+      personName.innerHTML = `<a id="moveText" href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
     }
-
-    personPicture.innerHTML = `<img src="${pictureDisplay}" style='width: 200px; height: auto'>`;
-    personName.innerHTML = `<a href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
-    personBio.innerHTML = data.extract;
   } catch (error) {
+    console.log(error);
     personName.innerHTML = `Oups ! Cette personne ne semble pas exister...`;
     personPicture.innerHTML = `<img src="./icons/giphy.gif" style='width: 200px; height: auto'>`;
     personBio.innerHTML = "";
@@ -54,6 +78,7 @@ myButton.addEventListener("click", async (e) => {
 
   try {
     await apiWiki(resultFormatName);
+    document.getElementById("personName").value = "";
   } catch (error) {
     console.error("Error:", error);
   }

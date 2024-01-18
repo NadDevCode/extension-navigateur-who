bdd7b92e6a926440551c7b4da4b40ea5a210fb8c
nad_cat <130969010+npelcat@users.noreply.github.com>
2024-01-18 15:38:54 +0100

    delete switch & ajout condition Kévin

--- script.js
+++ script.js
@@ -7,58 +7,36 @@ const personName = document.getElementById("name");
 const personBio = document.getElementById("description");
 
 async function apiWiki(name) {
-  let urlApi = `https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`;
+  if (name === "Kévin_Leiba") {
+    personName.innerHTML = `<h2>Kévin Leiba</h2>`;
+    personPicture.innerHTML = `<img src="https://ca.slack-edge.com/TFLNG6MFU-U05QMC5AVM1-5db909c58838-512" style='width: 200px; height: auto; border-radius: 10px'>`;
+    personBio.innerHTML = `Expert en développement web, se démarque par sa maîtrise inégalée de JavaScript. Passionné et énergique, il insuffle une créativité débordante à chaque ligne de code. Son amour pour le sucre syntaxique est à la fois une signature artistique et une source d'inspiration constante. Actif dans le monde open source, Kévin jongle avec les bibliothèques et frameworks tout en maintenant une productivité exceptionnelle, alimentée par une montagne de snacks sucrés. En résumé, Kévin Leiba transcende les normes du développement, combinant expertise technique, créativité, et une énergie infatigable.`;
+  } else {
+    let urlApi = `https://fr.wikipedia.org/api/rest_v1/page/summary/${name}`;
 
-  try {
-    const response = await fetch(urlApi);
-    console.log(response);
+    try {
+      const response = await fetch(urlApi);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+      const data = await response.json();
+      console.log(data.originalimage);
 
-    const data = await response.json();
-    console.log(data.originalimage);
-
-    if (data.originalimage) {
-      let pictureDisplay;
-      let bioDisplay;
-
-      switch (name) {
-        case "Eric_Zemmour":
-          pictureDisplay = "https://urlz.fr/pfmI";
-          bioDisplay =
-            "Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration ImmigrationImmigration Immigration  Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration Immigration ImmigrationImmigration  Immigration Immigration Immigration Immigration Immigration Immigration Immigration";
-          break;
-        case "Cyril_Hanouna":
-          pictureDisplay = "https://urlr.me/MvbCP";
-          bioDisplay = `né le 23 septembre 1974
-          Animateur incontesté du chaos médiatique. Avec une subtilité aussi rare qu'une licorne rose, Hanouna a réussi à transformer chaque émission en un « spectacle » où la décence et la logique font leurs adieux.ssion en un « spectacle » où la décence et la logique font leurs adieux.`;
-          break;
-        case "Emmanuel_Macron":
-          pictureDisplay =
-            "https://confpeps.org/wp-content/uploads/2023/03/macron-doigt-retraite-750x465.jpg";
-          break;
-        default:
-          pictureDisplay = data.originalimage.source;
-          bioDisplay = data.extract;
+      if (data.originalimage) {
+        personPicture.innerHTML = `<img src="${data.originalimage.source}" style='width: 200px; height: auto; border-radius: 10px'>`;
+        personName.innerHTML = `<a href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
+        personBio.innerHTML = data.extract;
+      } else {
+        personBio.innerHTML = `<i id="hand" class="fa-solid fa-hand-pointer"></i> <br/> Il existe plusieurs liens pour ce nom. Débrouillez-vous !`;
+        personName.innerHTML = `<a id="moveText" href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
+        personPicture.innerHTML = "";
       }
+    } catch (error) {
+      console.log(error);
+      personName.innerHTML = `Oups ! Cette personne ne semble pas exister...`;
+      personPicture.innerHTML = `<img src="./icons/giphy.gif" style='width: 200px; height: auto'>`;
+      personBio.innerHTML = "";
 
-      personPicture.innerHTML = `<img src="${pictureDisplay}" style='width: 200px; height: auto; border-radius: 10px'>`;
-      personName.innerHTML = `<a href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
-      personBio.innerHTML = bioDisplay;
-    } else {
-      personBio.innerHTML = `<i id="hand" class="fa-solid fa-hand-pointer"></i> <br/> Il existe plusieurs liens pour ce nom. Débrouillez-vous !`;
-      personName.innerHTML = `<a id="moveText" href="${data.content_urls.desktop.page}" target="_blank">${data.titles.normalized}</a>`;
-      personPicture.innerHTML="";
+      console.error("Error fetching data:" + error.message);
     }
-  } catch (error) {
-    console.log(error);
-    personName.innerHTML = `Oups ! Cette personne ne semble pas exister...`;
-    personPicture.innerHTML = `<img src="./icons/giphy.gif" style='width: 200px; height: auto'>`;
-    personBio.innerHTML = "";
-
-    console.error("Error fetching data:" + error.message);
   }
 }
 
@@ -72,13 +50,13 @@ const formatName = (inputValue) => {
   return nameForApi;
 };
 
-myButton.addEventListener("click", async (e) => {
+myButton.addEventListener("click", (e) => {
   e.preventDefault();
   const inputName = document.getElementById("personName").value;
   const resultFormatName = formatName(inputName);
 
   try {
-    await apiWiki(resultFormatName);
+    apiWiki(resultFormatName);
     document.getElementById("personName").value = "";
   } catch (error) {
     console.error("Error:", error);
